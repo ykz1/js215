4c738b3c5d25cd32cc405d809257f3f087120dca
Kyle Zhao <kyle.y.zhao@gmail.com>
2025-11-26 06:08:11 +0900

    Rest of String Processing exercises

---
+++ exercises/string_processing/8.js
@@ -0,0 +1,24 @@
+function wordLengths(str) {
+  if (!str) return [];
+  let output = [];
+  str.split(/s+/).forEach(word => {
+    output.push(word + ' ' + word.length);
+  })
+  return output;
+}
+
+
+console.log(wordLengths('cow sheep chicken'));
+// ["cow 3", "sheep 5", "chicken 7"]
+
+console.log(wordLengths('baseball hot dogs and apple pie'));
+// ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+console.log(wordLengths("It ain't easy, is it?"));
+// ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+console.log(wordLengths('Supercalifragilisticexpialidocious'));
+// ["Supercalifragilisticexpialidocious 34"]
+
+console.log(wordLengths(''));      // []
+console.log(wordLengths());        // []
